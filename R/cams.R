93aa121db3c876544384a9ef1985e33ecb15b874
slackline <nshephard@gmail.com>
2016-01-04 07:55:42 +0000

    Added code for turning size into factor variable

--- R/cams.R
+++ R/cams.R
@@ -13,7 +13,7 @@ cams <- function(id        = '',
                  passwd    = '',
                  cam.doc   = 'Cam Size Data',
                  cam.sheet = 'Plain List (2015-12-19)',
-                 cams.df   = cams.df,
+                 df        = cams.df,
                  smooth    = 'loess',
                  ...){
     # Refresh data if id/password specified
@@ -24,29 +24,51 @@ cams <- function(id        = '',
         cams <- gs_title(cam.doc,
                          verbose = FALSE)
         # Read the data
-        cams.df <- gs_read(ss = cams,
+        df <- gs_read(ss = cams,
                            ws = cam.sheet)
-    # Rename
-        names(cams.df) <- gsub("_", ".", names(cams.df))
-    # Generate unique ID
-        cams.df <- mutate(cams.df,
-                          manufacturer.model      = paste(manufacturer, model, sep = " "),
-                          model.size              = paste(model, size, sep = " "),
-                          manufacturer.model.size = paste(manufacturer, model, size, sep = " "))
+        # Rename
+        names(df) <- gsub("_", ".", names(df))
+        # Generate unique ID
+        df <- mutate(df,
+                     manufacturer.model      = paste(manufacturer, model, sep = " "),
+                     model.size              = paste(model, size, sep = " "),
+                     manufacturer.model.size = paste(manufacturer, model, size, sep = " "))
+        # Create factor out of size
+        df$size <- factor(df$size,
+                          levels = c("000", "00", "0", "00/0", "0", "0/1",
+                                     "0.1", "0.2", "0.3", "1/3", "1/3 to 3/8",
+                                     "3/8", "3/8 to 1/2", "0.4", "0.5", "1/2",
+                                     "1/2 to 3/4", "0.6", "0.75", "3/4",
+                                     "3/4 to 7/8", "0.8", "4/5", "0.85",
+                                     "7/8", "7/8 to 1", "0.95", "1", "1.25",
+                                     "1.5", "1.75", "2", "2.5", "3", "3.5",
+                                     "4", "5", "6", "7", "8", "9", "Small",
+                                     "Medium", "Large"))
     }
     results <- list()
     # Plot every cam
-    results$all.range <- dplyr::select(cams.df, manufacturer.model, manufacturer.model.size, lower, upper) %>%
-                         melt(id.vars = c("manufacturer.model", "manufacturer.model.size"))
+    results$all.range <- dplyr::select(df, manufacturer.model, size, manufacturer.model.size, lower, upper) %>%
+                         melt(id.vars = c("manufacturer.model", "size", "manufacturer.model.size"))
     results$all <- ggplot(results$all.range,
                           aes(value,
                               manufacturer.model.size)) +
         geom_line(aes(colour = manufacturer.model)) +
         xlab("Range (mm)") +
         ylab("Cam (Manufacturer / Model / Size)") +
-        theme(legend.position = "none")
+        theme(legend.position = "none",
+              axis.text.y = element_text(size  = 8))
+    results$all.manufacturer <- ggplot(results$all.range,
+                          aes(value,
+                              size)) +
+        geom_line(aes(colour = manufacturer.model)) +
+        xlab("Range (mm)") +
+        ylab("Cam (Model / Size)") +
+        facet_grid(manufacturer.model ~ ., scales = "free_y") +
+        theme(legend.position = "none",
+              axis.text.y = element_text(size  = 8),
+              strip.text.y = element_text(angle = 0))
     # Range covered by a manufacturers model
-    results$model.range <- dplyr::select(cams.df, manufacturer.model, lower, upper) %>%
+    results$model.range <- dplyr::select(df, manufacturer.model, lower, upper) %>%
                            group_by(manufacturer.model) %>%
                            summarise(lower = min(lower),
                                      upper = max(upper)) %>%
@@ -59,7 +81,7 @@ cams <- function(id        = '',
         ylab("Cam (Manufacturer / Model)") +
         theme(legend.position = "none")
     # Facet by manufacturer
-    ## results$all.manufacturer <- ggplot(cams.df,
+    ## results$all.manufacturer <- ggplot(df,
     ##                                    aes(range,
     ##                                        model.size)) +
     ##     geom_line() +
@@ -67,21 +89,25 @@ cams <- function(id        = '',
     ##     ylab("Cam (Manufacturer / Model)") +
     ##     facet_grid(manufacturer ~.)
     # Strength v Range
-    results$range.strength <- ggplot(cams.df,
+    results$range.strength <- ggplot(df,
                                      aes(range,
                                          strength.active.max)) +
         geom_point(aes(colour = factor(manufacturer.model))) +
         geom_smooth(method = smooth, size = 1) + 
-        xlab("Range (mm)") + ylab("Max Active Strength (kN)")
+        xlab("Range (mm)") +
+        ylab("Max Active Strength (kN)") +
+        theme(legend.position = "none")
     # Strength v Weight
-    results$range.weight <- ggplot(cams.df,
+    results$range.weight <- ggplot(df,
                                    aes(range,
                                        weight)) +
         geom_point(aes(colour = factor(manufacturer.model))) +
         geom_smooth(method = smooth, size = 1) + 
-        xlab("Range (mm)") + ylab("Max Active Weight (kN)")
+        xlab("Range (mm)") +
+        ylab("Weight (g)") +
+        theme(legend.position = "none")
     # Return summary data frame by manjfacturer/model
-    results$summary.df <- summary.df <- group_by(cams.df,
+    results$summary.df <- summary.df <- group_by(df,
                                                  manufacturer.model) %>%
                           summarise(n            = n(),
                                     min.size     = min(lower),
@@ -94,6 +120,6 @@ cams <- function(id        = '',
                                     axels        = mean(axels),
                                     lobes        = mean(lobes))
     # Return results
-    results$cams.df <- cams.df
+    results$df <- df
     return(results)
 }

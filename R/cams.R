1aea1229a8f5df94f57bf870e140c9ae6433bd30
slackline <nshephard@gmail.com>
2016-02-17 22:26:12 +0000

    split methods of plotting by into range and (manufacturers) number

--- R/cams.R
+++ R/cams.R
@@ -86,19 +86,31 @@ cams <- function(id          = '',
                                      "3/4 to 7/8", "3/4 to 1",
                                      "7/8 to 1"))
         # Align cams based on minimum and maximum size
-        df$compare <- ""
+        df$by.range  <- ""
+        df$by.number <- ""
         df <- within(df, {
-                     compare[lower < 11]                <- 1
-                     compare[lower >= 11 & lower < 14]  <- 2
-                     compare[lower >= 14 & lower < 16]  <- 3
-                     compare[lower >= 16 & lower < 19]  <- 4
-                     compare[lower >= 19 & lower < 21]  <- 5
-                     compare[lower >= 21 & lower < 25]  <- 6
-                     compare[lower >= 25 & lower < 31]  <- 7
-                     compare[lower >= 31 & lower < 42]  <- 8
-                     compare[lower >= 42 & lower < 54]  <- 9
-                     compare[lower >= 54 & lower < 188] <- 10
-                     compare[lower >= 188]              <- 11
+                     by.range[lower < 11]                 <- 1
+                     by.range[lower >= 11 & lower < 14]   <- 2
+                     by.range[lower >= 14 & lower < 16]   <- 3
+                     by.range[lower >= 16 & lower < 19]   <- 4
+                     by.range[lower >= 19 & lower < 21]   <- 5
+                     by.range[lower >= 21 & lower < 25]   <- 6
+                     by.range[lower >= 25 & lower < 31]   <- 7
+                     by.range[lower >= 31 & lower < 42]   <- 8
+                     by.range[lower >= 42 & lower < 54]   <- 9
+                     by.range[lower >= 54 & lower < 188]  <- 10
+                     by.range[lower >= 188]               <- 11
+                     ## by.number[size = '']  <- 1
+                     ## by.number[size = '']  <- 2
+                     ## by.number[size = '']  <- 3
+                     ## by.number[size = '']  <- 4
+                     ## by.number[size = '']  <- 5
+                     ## by.number[size = '']  <- 6
+                     ## by.number[size = '']  <- 7
+                     ## by.number[size = '']  <- 8
+                     ## by.number[size = '']  <- 9
+                     ## by.number[size = '']  <- 10
+                     ## by.number[size = '']  <- 11
         })
     }
     # ToDo - Take list compare and filter()

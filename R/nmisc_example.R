b0d49b5690d9f7254bc483e38aa9d539bf186d2c
slackline <nshephard@gmail.com>
2016-04-16 22:28:46 +0100

    Added display.mode as an option

--- R/nmisc_example.R
+++ R/nmisc_example.R
@@ -1,15 +1,17 @@
 #' Runs Shiny for Cam comparison
 #'
 #' @param example The Shiny example you wish to run (omit for a list of possible options).
+#' @param display.mode Shiny display mode to use \code{normal} (default) \code{showcase}.
 #' @export
-nmisc_example <- function(example = 'cams') {
+nmisc_example <- function(example = 'cams',
+                          display.mode = 'normal') {
     # locate all the shiny app examples that exist
     valid_examples <- list.files(system.file("shiny", package = "nmisc"))
     valid_examples_msg <-
         paste0("Valid examples are: '",
                paste(valid_examples, collapse = "', '"),
                "'")
-    
+
     # if an invalid example is given, throw an error
     if (missing(example) || !nzchar(example) ||
         !example %in% valid_examples){
@@ -19,5 +21,5 @@ nmisc_example <- function(example = 'cams') {
     }
     # find and launch the app
     app_dir <- system.file("shiny", example, package = "nmisc")
-    shiny::runApp(app_dir, display.mode = "normal")
+    shiny::runApp(app_dir, display.mode = display.mode)
 }

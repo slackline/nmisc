0d6305c554c1753dac34c307ea5dc25fdcd5a0d3
slackline <nshephard@gmail.com>
2016-05-25 23:50:02 +0100

    Halfway there on repairing the damage

--- inst/shiny/cams/server.R
+++ inst/shiny/cams/server.R
@@ -27,14 +27,41 @@ shinyServer(function(input, output){
                                       manufacturer.model.size %in% input$to.compare.manufacturer.range)
                            }
                            else if(!is.null(input$to.compare.manufacturer.number)){
+                               if(input$to.compare.manufacturer.number == 'Offsets'){
+                                   filter(cams.df,
+                                          manufacturer.model.size %in% c('00/0', '0/1', '1/2 to 3/4', '1/2', '1/3', '1/3 to 3/8', '2/3', '3/4 to 1', '3/4 to 7/8', '3/4', '3/8', '3/8 to 1/2', '4/5'))
+                               }
+                               else if(input$to.compare.manufacturer.number == '<1'){
                                filter(cams.df,
-                                      manufacturer.model.size %in% input$to.compare.manufacturer.number)
+                                      manufacturer.model.size %in% c('0', '0.1', '0.2', '0.25', '0.3', '0.4', '0.5', '0.6', '0.65', '0.7', '0.75', '0.8', '0.85', '0.95', '00', '000', '1'))
+                               }
+                               else if(input$to.compare.manufacturer.number == '1-2'){
+                               filter(cams.df,
+                                      manufacturer.model.size %in% c('1', '1.25', '1.5', '1.75', '1.80', '12', '2', 'Small'))
+                               }
+                               else if(input$to.compare.manufacturer.number == '2-3'){
+                               filter(cams.df,
+                                      manufacturer.model.size %in% c('2', '2.5', '2/3', '3'))
+                               }
+                               else if(input$to.compare.manufacturer.number == '3-4'){
+                               filter(cams.df,
+                                      manufacturer.model.size %in% c('3', '3.5', '4'))
+                               }
+                               else if(input$to.compare.manufacturer.number == '4-5'){
+                               filter(cams.df,
+                                      manufacturer.model.size %in% c('4', '5'))
+                               }
+                               else if(input$to.compare.manufacturer.number == '>5'){
+                               filter(cams.df,
+                                      manufacturer.model.size %in% c('6', '7', '7/8', '7/8', 'to', '1', '8', '9'))
+                               }
                            }
                            else if(!is.null(input$to.compare.manufacturer.model.size)){
                                filter(cams.df,
                                       manufacturer.model.size %in% input$to.compare.manufacturer.model.size)
                            }
-                           else(is.null(input$to.compare)){
+##                           else if(is.null(input$to.compare)){
+                           else{
                                cams.df
                            }
     })

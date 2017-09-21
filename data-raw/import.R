3be3ae22b8828c3884d56e69ddd503f4a1877df1
slackline <nshephard@gmail.com>
2017-09-21 12:06:11 +0100

    Sorting grades; ToDo notes

--- data-raw/import.R
+++ data-raw/import.R
@@ -3,13 +3,46 @@ ukc_logbook <- read_csv(file = 'csv/slacky_Logbook.csv')
 names(ukc_logbook) <- c('route', 'grade', 'style', 'partner', 'notes', 'date', 'crag')
 ukc_logbook <- cbind(ukc_logbook,
                      str_locate(ukc_logbook$grade, pattern = ' \\*+')) %>%
-               mutate(stars = substring(grade,
-                                        start,
-                                        end),
-                      grade = substring(grade,
-                                        1,
-                                        start - 1),
+               mutate(stars  = substring(grade,
+                                         start,
+                                         end),
+                      grade_ = substring(grade,
+                                         1,
+                                         start - 1),
+                      grade_ = gsub(' $', '', grade_),
+                      ## If route does not have any stars the grade
+                      ## needs replacing
+                      grade_ = ifelse(!is.na(grade_),
+                                      yes = grade_,
+                                      no  = grade),
+                      ## A few grades need manually sorting
+                      grade_ = ifelse(grade_ == '1',
+                                      yes = 'M',
+                                      no  = grade_),
+                      grade_ = ifelse(grade_ == '6c+ 2',
+                                      yes = '6c+',
+                                      no  = grade_),
+                      ## grade  = factor(grade_,
+                      ##                 levels = c('M', 'D', 'HD',
+                      ##                            'VD', 'S,)),
+                      ## ToDo - Determine the type of climb based on the grade
+                      ##
+                      ##        Trad
+                      ##        Bouldering
+                      ##        Sport
+                      ##        Multi-pitch?
+                      ##        Winter
+                      ##
+                      ## type   = case_when(grade == )
+                      ## ToDo - Substitute '\n' for ';'
                       ## notes = gsub(notes, '', ';'),
-                      date  = dmy(date))
+                      date   = dmy(date)) ## %>%
+                ## dplyr::select(-grade_)
+save(ukc_logbook, file = '../data/ukc_logbook.RData')
+
 head(ukc_logbook)
-save(ukc_logbook, '../data/ukc_logbook.RData')
+dplyr::filter(ukc_logbook, route == 'Angle Rib') %>%
+    dplyr::select(route, grade, grade_, date)
+
+dplyr::filter(ukc_logbook, grade_ == '6c+ 2') %>%
+    dplyr::select(route, grade, grade_, date)

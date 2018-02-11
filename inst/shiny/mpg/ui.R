17d4ca5b3d7566c294de98a3784a9fd63d9fa03d
slackline <nshephard@gmail.com>
2018-02-11 11:06:16 +0000

    Adding options

--- inst/shiny/mpg/ui.R
+++ inst/shiny/mpg/ui.R
@@ -1,9 +1,35 @@
 ui <- fluidPage(theme = shinytheme("flatly"),
                 ## Header
-                dashboardHeader(title = "Fuel Efficency"),
+                dashboardHeader(title = h1("Fuel Efficency")),
                 ## Sidebar
                 dashboardSidebar(
                     sidebarMenu(
+                        ## menuItem("File Upload", tabName = "upload", icon = icon("th")),
+                        fileInput("file1", "Choose CSV File",
+                                  accept = c("text/csv",
+                                             "text/comma-separated-values,text/plain",
+                                             ".csv")),
+                        checkboxInput("header", "Header", TRUE),
+                        # Input: Select separator ----
+                        radioButtons("sep", "Separator",
+                                     choices = c(Comma = ",",
+                                                 Semicolon = ";",
+                                                 Tab = "\t"),
+                                     selected = ","),
+                        # Input: Select quotes ----
+                        radioButtons("quote", "Quote",
+                                     choices = c(None = "",
+                                                 "Double Quote" = '"',
+                                                 "Single Quote" = "'"),
+                                     selected = '"'),
+                        radioButtons("distance", "Distance",
+                                     choices = c(Miles = "mpg",
+                                                 Kilometers = "km"),
+                                     selected = "mpg"),
+                        radioButtons("volume", "Volume",
+                                     choices = c(Litres = "l",
+                                                 Gallon = "g"),
+                                     selected = "l"),
                         menuItem("Price", tabName = "price", icon = icon("dashboard")),
                         menuItem("MPG", tabName = "mpg", icon = icon("dashboard")),
                         menuItem("KPL", tabName = "kpl", icon = icon("dashboard")),

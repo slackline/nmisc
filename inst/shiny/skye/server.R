71173ed38c5b1aa3825118fb3ffa0c24983e7c4c
slackline <nshephard@gmail.com>
2018-03-28 22:01:44 +0100

    a start

--- inst/shiny/skye/server.R
+++ inst/shiny/skye/server.R
@@ -3,19 +3,16 @@ server <- function(input, output, session){
     ## Raw Leaflet Map
     output$hill_leaflet <- renderLeaflet({
         ## Subset the data based on user specified selection
-        dplyr::filter(skye_cullin_gpx, hill == input$hill) %>%
-        leaflet() %>%
-            addProviderTiles(providers$OpenStreetMap,
-                             options = providerTileOptions(noWrap = TRUE)
-                             ) %>%
-            addCircleMarkers(radius      = 2,
-                             color       = "navy",
-                             fillOpacity = 0.2,
-                             popup       = ~htmlEscape(date_time))
-                                 ## paste0("Latitude  : ", ~htmlEscape(lat), "\n",
-                                 ##            "Longitude : ", ~htmlEscape(lng), "\n",
-                                 ##            "Speed     : ", ~htmlEscape(speed), "\n",
-                                 ##            "Bearing   : ", ~htmlEscape(bearing), "\n",
-                                 ##            "Height    : ", ~htmlEscape(height), "\n"))
+        if(input$hill != "All"){
+            df <- dplyr::filter(skye_cullin_gpx, hill == input$hill)
+        }
+        df %>%
+            leaflet() ## %>%
+            ## addProviderTiles(providers$OpenStreetMap,
+            ##                  options = providerTileOptions(noWrap = TRUE)) %>%
+            ## addCircleMarkers(radius      = 2,
+            ##                  color       = "navy",
+            ##                  fillOpacity = 0.2,
+            ##                  popup       = ~htmlEscape(ele))
     })
 }
